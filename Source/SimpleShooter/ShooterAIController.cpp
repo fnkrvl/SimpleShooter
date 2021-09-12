24bd9f23a91bf3b73577026d77ba7d82b4ad0356
Palms <franco.rvl91@gmail.com>
2021-09-12 10:32:34 -0300

    AI Controller

---
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AIBehavior != nullptr)
+	{
+		RunBehaviorTree(AIBehavior);
+	
+		const APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+
+		UE_LOG(LogTemp, Warning, TEXT("BT Works!"))
+		
+	}
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	APawn* PlayerPawn =	UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	
+	if (LineOfSightTo(PlayerPawn))
+	{
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+	}
+	else
+	{
+		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	}
+}
